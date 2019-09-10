5330cacda334dc3958613d382053a84c6353807c
Paulo Henrique <neomatrixgames@gmail.com>
2019-09-09 22:14:35 -0300

    testes instrumentado

--- app/src/androidTest/java/com/neomatrix/appbank/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/neomatrix/appbank/ExampleInstrumentedTest.java
@@ -3,11 +3,20 @@ package com.neomatrix.appbank;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -23,5 +32,22 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.neomatrix.appbank", appContext.getPackageName());
+    }
+
+
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void calculoImcActivity(){
+
+        onView (withId(R.id.user_login)).perform(typeText("test"));
+
+        onView (withId(R.id.user_password)).perform(typeText("Tes@1"));
+
+        onView(withId(R.id.login_button)).perform(click());
+
+
     }
 }
